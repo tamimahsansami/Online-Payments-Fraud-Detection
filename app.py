f8cdf9f684e7b614950a7ad3b015d1ad9c8bff83
Tamim Ahsan Sami <tamimahsan447@gmail.com>
2025-08-16 17:56:51 +0600

    This includes app.py and html file

--- app.py
+++ app.py
@@ -15,6 +15,14 @@ def home():
 @app.route('/predict_api', methods=['POST'])
 def predict_api():
     data = request.json['data']
+    type_mapping = {
+        "PAYMENT": 2,
+        "TRANSFER": 4,
+        "CASH_OUT": 1,
+        "DEBIT": 5,
+        "CASH_IN": 3
+    }
+    data['type'] = type_mapping.get(data['type'], -1)
     print(data)
     print(np.array(list(data.values())).reshape(1, -1))
     new_data = scalar.transform(np.array(list(data.values())).reshape(1, -1))
